44a4009e16896aa9e4f52aaf2e1a33349396a585
TheGamersHollow <thegamershollow@outlook.com>
2024-03-12 13:48:20 +0000

    Added Sprites for game

---
+++ Sprites/TileMaps etc/testing-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Game Boy GBC - Pokemon Red Blue - Tileset" tilewidth="8" tileheight="8" tilecount="8370" columns="54">
+ <image source="../Game Boy GBC - Pokemon Red Blue - Tileset.png" width="437" height="1240"/>
+</tileset>
